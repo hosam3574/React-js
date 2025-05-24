20a23c85af67afb84a10af097152250e7a31f435
hosam3574 <hossamtradat@gmail.com>
2025-05-24 15:41:42 +0300

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './component/nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
     <h1>
       Hi React Js
     </h1>
@@ -14,3 +16,4 @@ function App() {
 }
 
 export default App;
+
---
+++ my-app/src/component/nav.js
@@ -0,0 +1,30 @@
+import { useLayoutEffect } from "react"
+import '../css/nav.css'
+
+export default function Nav ( ){
+
+
+    return[
+        <div className ='nav' >
+        <ul>
+            
+           <li>Home </li>
+           
+
+ <li>About
+            </li>
+           
+
+            <li>
+                Contant
+            </li>
+
+        </ul>
+
+
+        </div>
+      
+   ]
+}
+
+
