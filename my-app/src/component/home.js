76a9840eed97b5016bf29abe22ba5ef51e375691
hosam3574 <hossamtradat@gmail.com>
2025-06-04 16:01:50 +0300

    react

--- my-app/src/component/home.js
+++ my-app/src/component/home.js
@@ -1,9 +1,22 @@
+import {useState }from "react"
+
 export default function Home ( ){
 
+const[data,setData]=useState ([
+    {Name : "Hosam" , age : 20 },
+ {Name : "Mouyyad" , age : 20 },
+ {Name : "Ali" , age : 20 }
+    ]);
 return(
 
     <div className="Home">
-<h1> Hosam Tradat</h1>
+<h1> My Name</h1>
+{data.map((item,index)=>(
+
+    <h1>
+        {index+1}:{item.name}and{item.age}
+    </h1>
+))}
 
     </div>
 )
